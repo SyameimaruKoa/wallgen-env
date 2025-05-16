268c765e30326824a70df15422e2e3e845610f32
SyameimaruKoa <toho.komeizi.aya@gmail.com>
2025-05-16 18:44:00 +0900

    れどめ修正(途中) リサイズの対象ファイルを自分で指定できるように修正

--- Wallpaper-Create_imgResize.sh
+++ Wallpaper-Create_imgResize.sh
@@ -3,36 +3,33 @@
 # 解像度を引数で指定する
 max_size=$1
 resize_mode=$2
+iPad_mode=$3
 
 # max_sizeが指定されていない場合はエラーを表示して終了する
 if [ -z "$max_size" ]; then
-  echo "リサイズする最大解像度を指定しておくれ。例: ./script.sh 2388 [iPad]"
+  echo "リサイズする最大解像度を指定しておくれ。例: ./script.sh 2388 [元拡張子] {iPad}"
   exit 1
 fi
 
-# 現在のディレクトリ名を取得して、その後ろに"_webp"または"_jpg"をつけたフォルダ名にする
+# 現在のディレクトリ名を取得して、その後ろに"引数で指定された拡張子をつけたフォルダ名にする
 dir_name=$(basename "$PWD")
-if [ "$resize_mode" == "iPad" ]; then
-  output_folder="./${dir_name}_webp"
-else
-  output_folder="./${dir_name}_jpg"
-fi
+  output_folder="./${dir_name}_${resize_mode}"
 
 # 出力フォルダが存在しない場合は作成する
 mkdir -p "$output_folder"
 
 # フォルダ内のPNG画像を取得
-png_files=("./"*.webp)
+png_files=("./"*.${resize_mode})
 total_files=${#png_files[@]}
 
 # PNGファイルが見つからない場合は終了
 if [ $total_files -eq 0 ]; then
-  echo "PNGファイルが見つからんぞ。"
+  echo "${resize_mode}ファイルが見つからんぞ。"
   exit 1
 fi
 
 # リサイズオプションを決定
-if [ "$resize_mode" == "iPad" ]; then
+if [ "$iPad_mode" == "iPad" ]; then
   resize_option="${max_size}x${max_size}>"
 else
   resize_option="x${max_size}"
@@ -42,7 +39,7 @@ fi
 count=0
 for img in "${png_files[@]}"; do
   # ファイル名を取得
-  filename=$(basename "$img" .png)
+  filename=$(basename "$img")
 
   # コマンドの存在確認
   if command -v magick >/dev/null 2>&1; then
@@ -55,7 +52,7 @@ for img in "${png_files[@]}"; do
   fi
 
   # 実行
-  if [ "$resize_mode" == "iPad" ]; then
+  if [ "$iPad_mode" == "iPad" ]; then
     $CMD "$img" -resize "$resize_option" -quality 95 "${output_folder}/${filename}.webp"
   else
     $CMD "$img" -resize "$resize_option" -quality 95 "${output_folder}/${filename}.jpg"
